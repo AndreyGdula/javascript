b00adb8bc5ce35a2afddca6f10d25943e7467f99
AndreyGdula <andreygdula@gmail.com>
2024-02-06 15:10:14 -0300

    módulo D

---
+++ cev/aula11/ex008.js
@@ -0,0 +1,7 @@
+var v = 120
+
+console.log(`A velocidade  informada foi ${v} km/h`)
+if (v > 120) {
+    console.log('Você foi multado!!')
+}
+console.log('Dirija com segurança')
---
+++ cev/aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = "Brasil"
+
+console.log(`País: ${pais}`)
+if (pais == "Brasil" || pais == "Br") {
+    console.log('Pessoa é brasileira')
+} else {
+    console.log('Estrangeiro')
+}
