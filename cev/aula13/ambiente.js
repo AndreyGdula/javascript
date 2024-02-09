2a276463a1d3e32580b27a9f5b589b6d8a02513b
AndreyGdula <andreygdula@gmail.com>
2024-02-09 15:43:02 -0300

    módulo E

---
+++ cev/aula13/ambiente.js
@@ -0,0 +1,5 @@
+var c = 1
+do { // Estrutura de repetição no final
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
---
+++ cev/aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('INÌCIO...')
+for (var c = 1; c <= 10; c++) {
+    console.log(c)
+}
+console.log('FIM...')
